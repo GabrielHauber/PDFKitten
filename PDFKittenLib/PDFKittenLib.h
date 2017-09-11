7ccc6649087480387a9d4e9e015ed50ef60ff837
Gabriel Hauber <gabriel@ghostdust.com>
2017-09-11 11:01:55 +1000

    Further configuration public headers

--- PDFKittenLib/PDFKittenLib.h
+++ PDFKittenLib/PDFKittenLib.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char PDFKittenLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PDFKittenLib/PublicHeader.h>
 
-#import <PDFKitten/PDFKitten.h>
+#import <PDFKittenLib/PDFKitten.h>
